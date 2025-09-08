125c031085dbed4ebc583ad5c9e2f1ab326f6991
mgac32124u18017-commits <mgac32124u18017@gmail.com>
2025-09-08 13:19:57 +0530

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,11 @@
+@media (max-width: 768px) {
+  .gallery {
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  }
+}
+
+@media (max-width: 480px) {
+  .gallery {
+    grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+  }
+}
